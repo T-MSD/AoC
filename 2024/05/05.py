30a9b874b3878a8747a645d67aa7b2401de3f1f3
T-MSD <dias.tiago03@gmail.com>
2024-12-11 02:04:28 +0000

    read_input now reads the full input

--- 2024/05/05.py
+++ 2024/05/05.py
@@ -1,12 +1,26 @@
 def read_input(file):
   rules = {}
+  updates = []
 
   with open(file, 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-      num1, num2 = line.strip().split("|")
-      rules[int(num1)] = int(num2)
-  return rules
+    sections = file.read().strip().split("\n\n")
+    rule_lines = sections[0].split("\n")
+    array_lines = sections[1].split("\n")
+
+    for line in rule_lines:
+      num1, num2 = line.split("|")
+      rules.setdefault(num1, []).append(num2)
+
+    for line in array_lines:
+      updates.append([int(num) for num in line.split(",")])
+
+    return rules, updates
+
+def is_ordered(rules, update):
+  
+  return True
 
 if __name__ == "__main__":
-  input = read_input('input.txt')
+  rules, updates = read_input('input.txt')
+  print(rules)
+  print(updates)
